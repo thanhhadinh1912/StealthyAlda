88792d4debf89c7f9bcc7819e41f9aab30c4c7fe
Thanh Ha Dinh <thanhha.dinh@smail.inf.h-brs.de>
2020-06-17 00:39:49 +0200

    print Hobby,Hardskill,Softskill for Student

--- src/main/java/com/stealthyalda/ai/control/ProfilStudentControl.java
+++ src/main/java/com/stealthyalda/ai/control/ProfilStudentControl.java
@@ -5,6 +5,16 @@
  */
 package com.stealthyalda.ai.control;
 
+import com.stealthyalda.ai.model.dao.HardskillDAO;
+import com.stealthyalda.ai.model.dao.HobbyDAO;
+import com.stealthyalda.ai.model.dao.SoftskillDAO;
+import com.stealthyalda.ai.model.entities.Benutzer;
+import com.stealthyalda.ai.model.entities.Hardskill;
+import com.stealthyalda.ai.model.entities.Hobby;
+import com.stealthyalda.ai.model.entities.Softskill;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author WINDOWS
@@ -13,5 +23,32 @@ public class ProfilStudentControl {
     public ProfilStudentControl(){
         
     }
+    public String printHardskill(Benutzer user){
+        String print="";
+        List<Hardskill> liste = new ArrayList<Hardskill>();
+        liste = HardskillDAO.getInstance().getHardskillsForUser(user);
+        for(int i=0;i<liste.size();i++){
+            print += liste.get(i).getHardskill() +"\n";
+}
+        return print;
+    }
+    public String printSoftskill(Benutzer user){
+        String print="";
+        List<Softskill> liste = new ArrayList<Softskill>();
+        liste = SoftskillDAO.getInstance().getSoftskillsForUser(user);
+        for(int i=0;i<liste.size();i++){
+            print += liste.get(i).getSoftskill() +"\n";
+}
+        return print;
+    }
     
+    public String printHobby(Benutzer user){
+        String print="";
+        List<Hobby> liste = new ArrayList<Hobby>();
+        liste = HobbyDAO.getInstance().getHobbysForUser(user);
+        for(int i=0;i<liste.size();i++){
+            print += liste.get(i).getHobby() +"\n";
+}
+        return print;
+    }
 }
