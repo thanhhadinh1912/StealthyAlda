d8a20cd7e267be85539b09d98d5d1d32f5f5af2e
Thanh Ha Dinh <thanhha.dinh@smail.inf.h-brs.de>
2020-06-24 13:49:37 +0200

    Duplications reduzieren

--- src/main/java/com/stealthyalda/ai/model/dtos/StellenanzeigeDTO.java
+++ src/main/java/com/stealthyalda/ai/model/dtos/StellenanzeigeDTO.java
@@ -5,50 +5,17 @@
  */
 package com.stealthyalda.ai.model.dtos;
 
+import com.stealthyalda.ai.model.entities.Stellenanzeige;
+
 import java.time.LocalDate;
 
 /**
  * @author WINDOWS
  */
-public class StellenanzeigeDTO {
-    private String titel;
-    private String beschreibung;
-    private String status;
-    private LocalDate datum;
+public class StellenanzeigeDTO extends Stellenanzeige {
     private String arbeitgeber;
-    private String ort;
 
-    public String getTitel() {
-        return titel;
-    }
 
-    public void setTitel(String titel) {
-        this.titel = titel;
-    }
-
-    public String getBeschreibung() {
-        return beschreibung;
-    }
-
-    public void setBeschreibung(String beschreibung) {
-        this.beschreibung = beschreibung;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public LocalDate getDatum() {
-        return datum;
-    }
-
-    public void setDatum(LocalDate datum) {
-        this.datum = datum;
-    }
 
     public String getArbeitgeber() {
         return arbeitgeber;
@@ -58,11 +25,5 @@ public class StellenanzeigeDTO {
         this.arbeitgeber = arbeitgeber;
     }
 
-    public String getOrt() {
-        return ort;
-    }
 
-    public void setOrt(String ort) {
-        this.ort = ort;
-    }
 }
