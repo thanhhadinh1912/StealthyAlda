e7625063f36796fdfc5629dfab5ad75fc0041d23
Thanh Ha Dinh <thanhha.dinh@smail.inf.h-brs.de>
2020-06-26 13:36:23 +0200

    BewerbungWindow verbessern

--- src/main/java/com/stealthyalda/gui/components/BewerbungWindow.java
+++ src/main/java/com/stealthyalda/gui/components/BewerbungWindow.java
@@ -10,21 +10,30 @@ public class BewerbungWindow extends Window {
         center();
         VerticalLayout content = new VerticalLayout();
         Label titel = new Label("<b> Bewerbung </b>", ContentMode.HTML);
+        titel.setWidth(WIDTH);
+
         content.addComponent(titel);
-        TextArea anschreiben = new TextArea();
+        content.setComponentAlignment(titel, Alignment.MIDDLE_CENTER);
+        TextArea anschreiben = new TextArea("Anschreiben");
         anschreiben.setWidth(WIDTH);
         anschreiben.setHeight(HEIGHT);
         content.addComponent(anschreiben);
+        content.setComponentAlignment(anschreiben,Alignment.MIDDLE_CENTER);
+
 
         TextArea erfahrung = new TextArea("Erfahrung");
         erfahrung.setHeight(HEIGHT);
         erfahrung.setWidth(WIDTH);
         content.addComponent(erfahrung);
+        content.setComponentAlignment(erfahrung,Alignment.MIDDLE_CENTER);
+
 
         TextArea zertifikat = new TextArea("Letzter Schulabschluss, Zertifikate, etc.");
         zertifikat.setWidth(WIDTH);
         zertifikat.setHeight(HEIGHT);
         content.addComponent(zertifikat);
+        content.setComponentAlignment(zertifikat,Alignment.MIDDLE_CENTER);
+
 
         HorizontalLayout button = new HorizontalLayout();
         button.setWidth(WIDTH);
@@ -32,16 +41,23 @@ public class BewerbungWindow extends Window {
         Button zuruck = new Button("Zurück");
         zuruck.setWidth("150px");
         button.addComponent(zuruck);
+        button.setComponentAlignment(zuruck, Alignment.MIDDLE_LEFT);
 
         Button abschicken = new Button("Abschicken");
         abschicken.setWidth("150px");
         button.addComponent(abschicken);
+        button.setComponentAlignment(abschicken,Alignment.MIDDLE_RIGHT);
 
         content.addComponent(button);
         content.setComponentAlignment(button,Alignment.MIDDLE_CENTER);
 
+        VerticalLayout main = new VerticalLayout();
+        main.setWidth("1000px");
 
-        this.setContent(content);
+        main.addComponent(content);
+        main.setComponentAlignment(content, Alignment.MIDDLE_CENTER);
+
+        this.setContent(main);
 
 
     }
