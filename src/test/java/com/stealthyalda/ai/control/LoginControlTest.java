7d31d080231288631875efc0aa697a516200bce1
Thanh Ha Dinh <thanhha.dinh@smail.inf.h-brs.de>
2020-06-30 18:34:41 +0200

    LoginControlTest ohne Mockito

--- src/test/java/com/stealthyalda/ai/control/LoginControlTest.java
+++ src/test/java/com/stealthyalda/ai/control/LoginControlTest.java
@@ -1,16 +1,30 @@
 package com.stealthyalda.ai.control;
 
+import com.stealthyalda.ai.control.exceptions.DatabaseException;
+import com.stealthyalda.ai.control.exceptions.NoSuchUserOrPassword;
 import org.junit.Test;
 
-class LoginControlTest {
+public class LoginControlTest {
+
+    @Test (expected = NoSuchUserOrPassword.class)
+    public void checkAuthenticationNeg2() throws NoSuchUserOrPassword, DatabaseException {
+        //unregistered user should fail
+        LoginControl.checkAuthentification("nicht null", "nicht null");
+    }
+
+
+    @Test (expected = NullPointerException.class)
+    public void logoutUserNeg(){
+        LoginControl.logoutUser();
+    }
 
 
     @Test
-    void checkAuthentification() {
+    public void checkAuthentification() {
 
     }
 
     @Test
-    void logoutUser() {
+    public void logoutUser() {
     }
 }
