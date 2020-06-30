bfee76519dd99bccf4eb7173ab792e39a0218a6f
Thanh Ha Dinh <thanhha.dinh@smail.inf.h-brs.de>
2020-06-30 02:16:36 +0200

    ToogleFeatureDAOTest

---
+++ src/test/java/com/stealthyalda/ai/model/dao/ToogleFeatureDAOTest.java
@@ -0,0 +1,22 @@
+package com.stealthyalda.ai.model.dao;
+
+import com.stealthyalda.ai.control.exceptions.DatabaseException;
+import com.stealthyalda.ai.model.entities.Arbeitgeber;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ToogleFeatureDAOTest {
+    ToogleFeatureDAO dao;
+    String feature;
+    @Before
+    public void setup(){
+        dao = ToogleFeatureDAO.getInstance();
+        feature = "bewerbung";
+    }
+    @Test
+    public void checkFeature() throws DatabaseException {
+        assertTrue(dao.checkFeature(feature));
+    }
+}
