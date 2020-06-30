57e861cb49a24be06059097c041da109a065204d
Erik <erik.flossbach@smail.inf.h-brs.de>
2020-06-30 20:43:13 +0200

    Role Tests

---
+++ src/test/java/com/stealthyalda/ai/model/dtos/RoleTest.java
@@ -0,0 +1,23 @@
+package com.stealthyalda.ai.model.dtos;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RoleTest {
+
+    @Test
+    public void getBezeichnung() {
+        Role r = new Role();
+        r.setBezeichnung("bez");
+        assertEquals("bez",r.getBezeichnung());
+
+    }
+
+    @Test
+    public void setBezeichnung() {
+        Role r = new Role();
+        r.setBezeichnung("bez");
+        assertEquals("bez",r.getBezeichnung());
+    }
+}
